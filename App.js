1c19f30332262eed8677f6af0792f5a09894d404
Jashwanth Roy Karnati <jroy.karnati@gmail.com>
2025-10-19 00:18:01 +0530

    Added scripts in pkg.json and started new React element

--- App.js
+++ App.js
@@ -1,3 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Heading created using react Element"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//For the Understanding of basics of react environment
 /*
  * <div id="Parent">
  *      <div id="child">
@@ -6,23 +18,20 @@
  * </div>
  */
 //Nested HTML structure
-import React from "react";
-import ReactDOM from "react-dom/client";
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", { id: "heading" }, "H1 Tag"),
+//     React.createElement("h2", { id: "heading2" }, "H2 Tag"),
+//   ])
+// );
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "H1 Tag"),
-    React.createElement("h2", { id: "heading2" }, "H2 Tag"),
-  ])
-);
-
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
-); // -> returns Object
-const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
-root.render(parent); //-> Makes the element to placed on the Top of DOM
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from React!"
+// ); // -> returns Object
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// console.log(root);
+// root.render(parent); //-> Makes the element to placed on the Top of DOM
