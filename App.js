22f12a8a6465538dc94d94274f79377ccf45e0b3
Jashwanth Roy Karnati <jroy.karnati@gmail.com>
2025-10-22 21:49:57 +0530

    Developed a Sample restaurant app for practice of Components concept in React

--- App.js
+++ App.js
@@ -1,32 +1,82 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//using JSX (just a syntax) also called React Element
-const heading = (
-  <h1 id="heading" className="root">
-    Heading Created using JSX
-  </h1>
+const Header = () => (
+  <div className="header">
+    <div className="Logo-container">
+      <img src="https://www.google.com/imgres?q=logo%20of%20a%20shopping%20mall&imgurl=https%3A%2F%2Fpng.pngtree.com%2Fpng-clipart%2F20200701%2Foriginal%2Fpngtree-shopping-mall-logo-png-image_5406131.jpg&imgrefurl=https%3A%2F%2Fpngtree.com%2Ffreepng%2Fshopping-mall-logo_5406131.html&docid=9Lm_PyiyFZh3rM&tbnid=-AZeTArA14F6CM&vet=12ahUKEwjT-qjllLiQAxXpzDgGHWK8NGYQM3oECCQQAA..i&w=1200&h=1200&hcb=2&ved=2ahUKEwjT-qjllLiQAxXpzDgGHWK8NGYQM3oECCQQAA" />
+    </div>
+    <div className="nav-items">
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  </div>
 );
+const RestroCard = () => (
+  <div className="restrocard">
+    <img className= "restrologo" alt="logo" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2Fp%2FPista-house-Kurnool-61566163440572%2F&psig=AOvVaw0izugAy4MqArwpcZXQfl15&ust=1761235741310000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCPDJlsiYuJADFQAAAAAdAAAAABAE"/>
+    <h3>Pista House</h3>
+    <h4>Biryani, Mandi, Top-Rated</h4>
+    <h4>4.3 stars</h4>
+    <h4>30mins delivery</h4>
+  </div>
+);
+
+const Body = () => (
+  <div className="body">
+    <div className="search">Search</div>
+    <div className="restrocontainer">
+      <RestroCard />
+      <RestroCard />
+      <RestroCard />
+      <RestroCard />
+      <RestroCard />
+      <RestroCard />
+      <RestroCard />
+      <RestroCard />
+    </div>
+  </div>
+);
+
+const AppLayout = () => (
+  <div className="app">
+    <Header />
+    <Body />
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
+
+//using JSX (just a syntax) also called React Element
+// const heading = (
+//   <h1 id="heading" className="root">
+//     Heading Created using JSX
+//   </h1>
+// );
 
 //React Component
 //Functional
 //Component COmposition
-const HeadingComponent = () => (
-  <div>
-    {heading}
-    {/* Three types of Calling of Component */}
-    <TitleComponent /> 
-    <TitleComponent></TitleComponent>
-    {TitleComponent()}
-    <h1>Heading Created using React Component</h1>
-  </div>
-);
-const TitleComponent = () => <h1 className="title">It is a title Component</h1>;
+// const HeadingComponent = () => (
+//   <div>
+//     {heading}
+//     {/* Three types of Calling of Component */}
+//     <TitleComponent />
+//     <TitleComponent></TitleComponent>
+//     {TitleComponent()}
+//     <h1>Heading Created using React Component</h1>
+//   </div>
+// );
+// const TitleComponent = () => <h1 className="title">It is a title Component</h1>;
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 // console.log(jsxheading,heading) Both are same
 // root.render(heading); for rendering React Element
-root.render(<HeadingComponent />);
+// root.render(<HeadingComponent />);
 
 //For the Understanding of basics of react environment
 /*
