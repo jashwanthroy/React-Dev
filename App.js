db7bbe9dee6ba6512803472376cd4f8005938f7f
Jashwanth Roy Karnati <jroy.karnati@gmail.com>
2025-10-22 19:19:04 +0530

    Calling React element in component and vice versa and Power of JSX and types of calling component

--- App.js
+++ App.js
@@ -1,59 +1,63 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
-//using JSX (just a syntax)
-const heading = (<h1 id="heading" className="root">Heading Created using JSX</h1>);
+//using JSX (just a syntax) also called React Element
+const heading = (
+  <h1 id="heading" className="root">
+    Heading Created using JSX
+  </h1>
+);
 
 //React Component
-const HeadingComponent = () =>{
-      return <h1>Heading Creted using React Component</h1>
-}
-
-
-
-
-
+//Functional
+//Component COmposition
+const HeadingComponent = () => (
+  <div>
+    {heading}
+    {/* Three types of Calling of Component */}
+    <TitleComponent /> 
+    <TitleComponent></TitleComponent>
+    {TitleComponent()}
+    <h1>Heading Created using React Component</h1>
+  </div>
+);
+const TitleComponent = () => <h1 className="title">It is a title Component</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // console.log(jsxheading,heading) Both are same
-root.render(heading);
-
-
-
-
+// root.render(heading); for rendering React Element
+root.render(<HeadingComponent />);
 
 //For the Understanding of basics of react environment
 /*
-* <div id="Parent">
-*      <div id="child">
-*          <h1></h1>
-*      </div>
-* </div>
-*/
+ * <div id="Parent">
+ *      <div id="child">
+ *          <h1></h1>
+ *      </div>
+ * </div>
+ */
 //Nested HTML structure
 // const parent = React.createElement(
-  //   "div",
-  //   { id: "parent" },
-  //   React.createElement("div", { id: "child" }, [
-    //     React.createElement("h1", { id: "heading" }, "H1 Tag"),
-    //     React.createElement("h2", { id: "heading2" }, "H2 Tag"),
-    //   ])
-    // );
-    
-    // const heading = React.createElement(
-      //   "h1",
-      //   { id: "heading" },
-      //   "Hello World from React!"
-      // ); // -> returns Object
-      // const root = ReactDOM.createRoot(document.getElementById("root"));
-      // console.log(root);
-      // root.render(parent); //-> Makes the element to placed on the Top of DOM
-      
-      
-      //Rendering using React.createElement and ReactDOM.createRoot
-      // const heading = React.createElement(
-        //   "h1",
-        //   { id: "heading" },
-        //   "Heading created using react Element"
-        // );
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", { id: "heading" }, "H1 Tag"),
+//     React.createElement("h2", { id: "heading2" }, "H2 Tag"),
+//   ])
+// );
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from React!"
+// ); // -> returns Object
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// console.log(root);
+// root.render(parent); //-> Makes the element to placed on the Top of DOM
+
+//Rendering using React.createElement and ReactDOM.createRoot
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Heading created using react Element"
+// );
